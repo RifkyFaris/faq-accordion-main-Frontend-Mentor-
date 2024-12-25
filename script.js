4d548c5dd886a57c84ea65c0bbc8b5e3b067fd68
Rifky Faris <78222881+RifkyFaris@users.noreply.github.com>
2024-12-25 14:11:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function plus1(){
+    document.getElementById("plus").style.display="none";
+    document.getElementById("minus").style.display="block";
+    document.getElementById("fm").style.display="block";
+
+}
+function minus1(){
+    document.getElementById("plus").style.display="block";
+    document.getElementById("minus").style.display="none";
+    document.getElementById("fm").style.display="none";
+
+}
+function plus2(){
+    document.getElementById("plus2").style.display="none";
+    document.getElementById("minus2").style.display="block";
+    document.getElementById("fm1").style.display="block";
+
+}
+function minus2(){
+    document.getElementById("plus2").style.display="block";
+    document.getElementById("minus2").style.display="none";
+    document.getElementById("fm1").style.display="none";
+}
+function plus3(){
+    document.getElementById("plus3").style.display="none";
+    document.getElementById("minus3").style.display="block";
+    document.getElementById("fm2").style.display="block";
+
+}
+function minus3(){
+    document.getElementById("plus3").style.display="block";
+    document.getElementById("minus3").style.display="none";
+    document.getElementById("fm2").style.display="none";
+}
+
+function plus4(){
+    document.getElementById("plus4").style.display="none";
+    document.getElementById("minus4").style.display="block";
+    document.getElementById("fm3").style.display="block";
+
+}
+function minus4(){
+    document.getElementById("plus4").style.display="block";
+    document.getElementById("minus4").style.display="none";
+    document.getElementById("fm3").style.display="none";
+}
